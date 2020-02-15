6037db418543f9a3d6e8007d61838c09bdfa6a47
alex <alex_kobilianskiy@bigmir.net>
2020-02-15 13:13:48 +0200

    section form complete

--- app/js/common.js
+++ app/js/common.js
@@ -104,6 +104,25 @@ $(document).ready(function(){
     heightses();
 
 
+    /** FORMS START */
+
+    $('.agreement').styler();
+
+    var uPhone = $('.user-phone');
+    uPhone.mask("+7 (999) 999-99-99",{autoclear: false});
+
+    uPhone.on('click', function (ele) {
+        var needelem = ele.target || event.srcElement;
+        needelem.setSelectionRange(4,4);
+        needelem.focus();
+    });
+
+    $.validate({
+        form : '.contact-form',
+        scrollToTopOnError: false
+    });
+
+
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
